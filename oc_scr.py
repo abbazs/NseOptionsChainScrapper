b777119797bc772b84b637625ab8db395de40a59
abbazs <abbazs@users.noreply.github.com>
2019-02-13 23:09:40 +0530

    Added function to get all expirys available

--- oc_scr.py
+++ oc_scr.py
@@ -5,22 +5,34 @@ from datetime import datetime
 
 start_url = 'https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&instrument=OPTIDX&symbol=BANKNIFTY'
 
-
-def get_chain():
+def get_expirys(url):
     try:
-        pg = requests.get(start_url)
+        pg = requests.get(url)
         bsf = BeautifulSoup(pg.content, 'html5lib')
-        tables = bsf.findAll('table')
+        exps = bsf.find('select', attrs={'name':'date'}).findAll('option')
+        expirys = []
+        for e in exps[1:]:
+            expirys.append(e.contents[0].strip())
+        return expirys
+    except Exception as e:
+        print(e)
+
+def get_chain(date):
+    try:
+        url = f'{start_url}&date={date}'
+        pg = requests.get(url)
+        bsf = BeautifulSoup(pg.content, 'html5lib')
         table = bsf.find("table", attrs={'id':'octable'})
         table.find('thead')('tr')[0].extract()
-        lrow = table('tbody')[0]('tr')[-1].extract()
         df = pd.read_html(table.prettify())
         df = df[0]
         df = df.replace('-', 0)
-        df.to_excel(f'banknifty_{datetime.now():%Y-%m-%d_%H-%M-%S}.xlsx')
+        df.to_excel(f'banknifty_{date}_{datetime.now():%Y-%m-%d_%H-%M-%S}.xlsx')
         print('Saved ...')
     except Exception as e:
         print(e)
 
 if __name__ == '__main__':
-    get_chain()
+    exps = get_expirys(start_url)
+    for d in exps:
+        get_chain(d)
